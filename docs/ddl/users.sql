5b2bfd5df8a32b48600383030bcdc75012b1cc6d
gekko555 <hide54242564@gmail.com>
2026-04-19 17:14:50 +0900

    プロジェクトで不要なファイルを削除、DBnoDDLとテストデータを作成

--- docs/ddl/users.sql
+++ docs/ddl/users.sql
@@ -1,12 +1,12 @@
 -- todo_app.users definition
 
 CREATE TABLE `users` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ユーザー名',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'パスワード',
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'メールアドレス',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日時',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
---
+++ docs/sql/users.sql
@@ -0,0 +1,8 @@
+-- users test data (5 records)
+
+INSERT INTO users (username, password, email) VALUES
+('管理者', 'password123', 'admin@example.com'),
+('田中', 'password123', 'tanaka@example.com'),
+('鈴木', 'password123', 'suzuki@example.com'),
+('山田', 'password123', 'yamada@example.com'),
+('佐藤', 'password123', 'sato@example.com');
